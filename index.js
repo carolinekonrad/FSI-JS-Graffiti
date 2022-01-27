4fbef0f2e5ccc18b6070a0e11e2135537279cd2c
Caroline Konrad <ckonrad@live.unc.edu>
2022-01-26 19:00:12 -0500

    first commit

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let header = document.querySelector('#page-header')
+header.style.textAlign = 'left'
+header.style.color = "magenta"
 
-// Select some elements...
+let dogImages = document.querySelectorAll('.dog-image')
+for(let i = 0; i < dogImages.length; i++){
+    dogImages[i].style.borderRadius = '50px'
+}
+
+let dogNames = document.querySelectorAll('.dog-name')
+dogNames.style.color = "white"
+
